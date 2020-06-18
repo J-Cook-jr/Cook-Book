5eba5c280a20a2a18d76a7d09f46976f659ad03c
Jeff Cook <jecook2692@gmail.com>
2020-06-18 10:43:36 -0400

    adds files and creates database

---
+++ migrations/20200618141417-create-recipes-to-categories.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+    return queryInterface.createTable('RecipeCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      recipeId : {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Recipes',
+          key: 'id'
+        },
+      },
+      categoriesId : {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        },
+      }
+    });
+  },
+
+
+    
+  
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('RecipesCategories');
+  }
+};
