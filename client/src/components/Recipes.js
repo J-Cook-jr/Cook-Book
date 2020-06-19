4434c0c322c2390d110d94400e594d3b092a07d7
Jeff Cook <jecook2692@gmail.com>
2020-06-19 14:36:21 -0400

    updates App.js file in React

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -13,13 +13,23 @@ export default class Recipes extends Component {
             ],
         }
     }
+    componentDidMount() {
+        fetch('/api/v1/recipes')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    recipes: data
+                })
+            })
+    }
     render() {
         return (
             <div>
                 {this.state.recipes.map(recipe => {
                     return (
                         <div>
-                            <pre>{ JSON.stringify(recipe, null, '\n') }</pre>
+                            <h1>{ recipe.name }</h1>
+                            <pre>{JSON.stringify(recipe, null, '\n') }</pre>
                         </div>
                     )
                 })}
