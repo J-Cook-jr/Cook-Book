545e39a4460dcaf2094b76b6917e0db751c29438
Jeff Cook <jecook2692@gmail.com>
2020-06-23 14:26:09 -0400

    adds recipe form and imports it to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Recipes from './components/Recipes';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
+import RecipeForm from './components/RecipeForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Recipes} />
+          <Route path ="/recipes/new" exact={RecipeForm} />
           <Route path="/recipes/:id" component={RecipeDetails} />
         </Switch>
       </div>
